00eb783e7846ab0166af98c61e964c79299f2744
Student <student@MBP-2009-13.localdomain>
2018-07-28 13:48:51 +0300

    first js lesson

---
+++ lesson7/index.js
@@ -0,0 +1,141 @@
+/* JavaScript code 
+   !!!!!!!!!!!!!!!
+*/
+
+// one line comment
+
+// var n1 = 100;
+// var n2 = 200;
+// var n3 = 1.5;
+
+// var n1 = 100,
+//     n2 = 200,
+//     n3 = 1.5;
+
+// var a = 50;
+
+// a = 100;
+
+// var numb = 200;
+// var name = 'Maksim';
+
+// var str = 'Lorem ipsum dolor';
+
+// var expression = true;
+// var expression2 = false;
+
+// var value = null;
+
+// var name = prompt('Enter your name: ');
+
+// console.log(name);
+
+// var a = 100;
+// var b = 150;
+// var c = 1.5;
+
+// var sum = a + b + c;
+
+// var o = a - b + c; 
+
+// console.log(sum);
+// console.log(o);
+
+// console.log(10 % 3);
+
+// var z = 5;
+
+// z = z + 1;
+// z += 1;
+// z++;
+// ++z;
+
+// console.log('hello\n world!')
+
+// a = a + 10;
+// a += 10;
+
+
+// var login = prompt('User name');
+
+// if (login === 'ADMIN') {
+//     console.log('hello admin');
+// } else {
+//     console.log('not admin');
+// }
+
+
+// var color = 'blue';
+
+// switch(color) {
+//     case 'red':
+//         console.log('1');
+//         break;
+//     case 'green':
+//         console.log('2');
+//         break;
+//     case 'blue':
+//         console.log('3');
+//         break;
+//     default:
+//         console.log('unknown');
+// }
+
+// 25 === 25 ? console.log('pravda') : console.log('not');
+
+// swap
+
+// var a = 5;
+// var b = 100;
+
+// console.log(a, b); // 5 100
+
+// var copy_of_a = a;
+
+// a = b;
+
+// b = copy_of_a;
+
+// //  your code
+
+// console.log(a, b); // 100 5
+
+// task2
+
+// var numb1 = parseInt(prompt('number1'));
+// var numb2 = parseInt(prompt('number2'));
+
+// var max;
+
+// if (numb1 !== numb2) {
+//     // max = numb1 > numb2 ? numb1 : numb2;
+//     if (numb1 > numb2) {
+//         max = numb1;
+//     } else {
+//         max = numb2;
+//     }
+
+//     console.log(max);
+// } else {
+//     console.log('Equal');
+// }
+
+// task3
+
+var n = -199;
+var module_n;
+
+if (n < 0) {
+    module_n = n * -1;
+} else {
+    module_n = n;
+}
+
+console.log(module_n);
+
+var x = 1,
+    z = 5;
+
+y = (x * x) + 3 * x * z - 0.5 * (z * z) - 12/17;
+
+alert(y);
